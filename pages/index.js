e9233edc73545b5db8b2473614daa1a0362b90fe
Aditya <rajaditya3759@gmail.com>
2023-04-21 08:19:08 +0530

    website all complete

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,25 @@
 import Head from "next/head";
 import HeroSection from "@/components/HeroSection";
 import BlogCard from "@/components/BlogCard";
+import { useState } from "react";
 
-export default function Home() {
+export default function Home(props) {
+  const [blog, setBlog] = useState(props.data);
   return (
     <>
       <Head>
         <title>Hunting Coders</title>
       </Head>
       <HeroSection />
+      <BlogCard blogData={blog} />
     </>
   );
+}
+
+export async function getServerSideProps(context) {
+  const res = await fetch("http://localhost:3000/api/blog");
+  const data = await res.json();
+  return {
+    props: { data }, // will be passed to the page component as props
+  };
 }
